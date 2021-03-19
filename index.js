b53c906be8657d01a778f5b159cccf6ca82171d4
Otor John Stephen <johnogenyi4real@gmail.com>
2021-03-19 20:06:24 +0100

    created the single product details page

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function menutoggle() {
         MenuItems.style.maxHeight = "0px";
     }
 }
+
+
+// -------------script for product gallery----------
+
+var productImg = document.getElementById('productImg');
+var smallImg = document.getElementsByClassName('small-img');
+
+    smallImg[0].onclick = function() {
+        productImg.src = smallImg[0].src;
+    }
+    smallImg[1].onclick = function() {
+        productImg.src = smallImg[1].src;
+    }
+    smallImg[2].onclick = function() {
+        productImg.src = smallImg[2].src;
+    }
+    smallImg[3].onclick = function() {
+        productImg.src = smallImg[3].src;
+    }
